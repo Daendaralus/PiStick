ed7cd91242c65ebea51f13695f54ec5ea41072b4
Lars Schlömer <lars.schloemer1@gmail.com>
2021-03-28 04:50:23 +0200

    asd

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import RPi.GPIO as GPIO
 NULL_CHAR = chr(0)
 GPIO.setmode(GPIO.BCM)
-inpins = {17:"a"}
+inpins = {17:'\x00\x00\x01\x00'}
 for k,v in inpins.items():
     GPIO.setup(k, GPIO.IN, GPIO.PUD_UP)
 
-import os, sys
+import os, sys, time
 fd = open("/dev/hidg0", "rb+")
-bytesWritten = fd.write( bytes([252, 8]))
-print('bytes written: ' + str(bytesWritten))
+
 while True:
     for k,v in inpins.items():
         if not GPIO.input(17):
-            fd.write((NULL_CHAR*2+chr(4)+NULL_CHAR*5).encode())
-            fd.write((NULL_CHAR*8).encode())
+            fd.write(v.encode())
+            time.sleep(0.05)
+            fd.write('\x00\x00\x00\x00'.encode())
+            fd.flush()
