13428c3e62c49f1488fe97e068bd4c45cf728b01
Lars Schlömer <lars.schloemer1@gmail.com>
2021-03-28 04:56:49 +0200

    asd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 NULL_CHAR = chr(0)
 GPIO.setmode(GPIO.BCM)
-inpins = {17:'\x00\x00\x01\x00'}
+inpins = {17:['\x00\x00\x01\x00',False]}
 for k,v in inpins.items():
     GPIO.setup(k, GPIO.IN, GPIO.PUD_UP)
 
@@ -13,7 +13,12 @@ def submit_action(action):
 
 while True:
     for k,v in inpins.items():
-        if not GPIO.input(17):
-            submit_action(v.encode())
-            time.sleep(0.05)
+        if not GPIO.input(k): #Key is down
+            if not v[1]:
+                submit_action(v.encode())
+                v[1] = True
+            time.sleep(0.01)
+            
+        else:
+            v[1] = False
             submit_action('\x00\x00\x00\x00'.encode())
