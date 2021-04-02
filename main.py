d9282aa72dc287297d2658093374fe0f4e7ae59a
Lars Schlömer <lars.schloemer1@gmail.com>
2021-04-02 12:12:04 +0200

    asd

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ inpins = {
     ,22:['\x00\x00\x10\x00',False] # Button 5
     ,9:['\x00\x00\x20\x00',False] # Button 6
     ,0:['\x00\x00\x40\x00',False] # Button 7
-    ,6:['\x00\x00\x80\x00',False] # Button 8
+    ,6:['\x00\x00\x80\x00',False] # Button 8 -> moves y axis to half
     ,19:['\x00\x00\x00\x01',False] # Button 9
     ,26:['\x00\x00\x00\x02',False] # Button 10
     ,21:['\x00\x00\x00\x04',False] # Button 11
@@ -16,7 +16,7 @@ inpins = {
     # ,15:['\x00\x00\x00\x10',False] # Button 13
     # ,14:['\x00\x00\x00\x20',False] # Button 14
     ,15:['\x00\x00\x00\x40',False] # Button 15
-    ,14:['\x00\x00\x00\x80',False]}# Button 16
+    ,14:['\x00\x00\x00\x80',False]}# Button 16 -> triggers 2,7,8,16
 
 for k,v in inpins.items():
     GPIO.setup(k, GPIO.IN, GPIO.PUD_UP)
@@ -27,15 +27,15 @@ import os, sys, time
 def submit_action(action):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(action)
-
-while True:
-    state = 0
+state = 0
+while True:    
     sendclean = True
     for k,v in inpins.items():
         if not GPIO.input(k): #Key is down
             if not v[1]:
                 state |= v[0]
                 #submit_action(v[0])
+                print(state, " | ", hex(state))
                 v[1] = True
             sendclean = False
             time.sleep(0.01)
